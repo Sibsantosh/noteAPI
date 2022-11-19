ba3fa23ede31a7529e33685176ab4054b90338ff
Blez_Ten <83898491+blezDev@users.noreply.github.com>
2022-11-20 01:39:17 +0530

    changed usercontroller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,37 +2,33 @@ const userModel = require("../models/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const SECRET_KEY = process.env.SECRET_KEY;
+const signup = async (req, res) =>{
 
+    const {username, email, password} = req.body;
+    try {
 
-const signup = async (req,res) =>{
-// Existing User Check
-// Hashed Password
-// User Creation
-// Token Generate
+        const existingUser = await userModel.findOne({ email : email});
+        if(existingUser){
+            return res.status(400).json({message: "User already exists"});
+        }
 
-const {email,password,username} = req.body;
-try{
-    const existingUser = await userModel.findOne({email : email}); // force the system halt until it is done
-    if(existingUser){
-        return res.status(400).json({message : "User already exist"});
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const result = await userModel.create({
+            email: email,
+            password: hashedPassword,
+            username: username
+        });
+
+        const token = jwt.sign({email : result.email, id : result._id }, SECRET_KEY);
+        res.status(201).json({user: result, token: token});
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: "Something went wrong"});
     }
-    const hashedPassword = await bcrypt.hash(password,10);
-    const result = await userModel.create({
-        email:email,
-        password : hashedPassword,
-        username : username
-    });
-    const token = jwt.sign({email : result.email, id : result.id},SECRET_KEY);
-    res.status(201).
-    json({message:"user added", user : result, token : token});
 
-}catch(error){
-console.log(error);
-res.status(500).json({message:"Something went wrong!!!"});
 }
-
-};
-
 
 const login = async (req,res)=>{
 
